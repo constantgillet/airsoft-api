a8bf1d22c83ceb0feec9c0dfa34e537a4173ed2c
DJouve <damien.jouve@hetic.net>
2020-12-16 17:03:47 +0100

    :lipstick: Fixing variables in scss

--- client/src/js/components/Footer/FooterElements/FooterElements.js
+++ client/src/js/components/Footer/FooterElements/FooterElements.js
@@ -4,7 +4,7 @@ import './FooterElements.scss'
 export default function FooterElements(props) {
     return (
         <div className="footer_elements">
-            <div className="title text-subtitle">{props.title}</div>
+            <div className="title">{props.title}</div>
             <div className="underline"></div>
             <p className="description">{props.description}</p>
         </div>
--- client/src/js/components/Header/Header.js
+++ client/src/js/components/Header/Header.js
@@ -8,10 +8,10 @@ export default function Header() {
                 <img src="/images/logo.png" className="logo_img" alt="logo_img"/>
             </div>
             <div className="link_container">
-                <a href="" className="link_header text-subtitle">Documentation</a>
-                <a href="" className="link_header text-subtitle">Status</a>
-                <a href="" className="link_header text-subtitle">Pricing</a>
-                <a href="" className="link_header text-subtitle link_blue">Contribuer</a>
+                <a href="" className="link_header">Documentation</a>
+                <a href="" className="link_header">Status</a>
+                <a href="" className="link_header">Pricing</a>
+                <a href="" className="link_header link_blue">Contribuer</a>
             </div>
         </div>
     )
