e95ef01ecf122fb44dae2556a1bdb6e21f8e4dd7
kilian-Marechal <kilian.marechal@hetic.net>
2020-12-16 18:15:17 +0100

    used fonts variables on homepage

--- client/src/js/components/TestComponent/DocComponent.js
+++ client/src/js/components/TestComponent/DocComponent.js
@@ -3,7 +3,8 @@ import './DocComponent.scss'
 
 export default function DocContainer({title, subtitle, bodyTitle, body1, body2, button}) {
   return (
-      <div className="doc-component">
+      <div className="doc-main-component">
+        <div className="doc-component">
           <div className="title-container">
             <h3 className="title">{title}</h3>
             <h5 className="subtitle">{subtitle}</h5>
@@ -12,10 +13,13 @@ export default function DocContainer({title, subtitle, bodyTitle, body1, body2, 
             <h5>{bodyTitle}</h5>
             <p>{body1}</p>
             <p>{body2}</p>
-            <div className="doc-button">
-                {button}
+            <div className="doc-button-container">
+              <div className="doc-button">
+                  {button}
+              </div>
             </div>
           </div>
+        </div>
       </div>
   )
 }
