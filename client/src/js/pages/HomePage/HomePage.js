a4c82f7c680cca303f5bbf3c0e99dbb90f1fc601
kilian-Marechal <kilian.marechal@hetic.net>
2020-12-16 15:20:30 +0100

    Added card components on homepage
    (both js and scss files)

--- client/src/js/pages/HomePage/HomePage.js
+++ client/src/js/pages/HomePage/HomePage.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import TestComponent from '../../components/TestComponent/TestComponent'
 import Header from '../../components/Header/Header'
+import CardComponent from '../../components/TestComponent/CardComponent'
 import Footer from '../../components/Footer/Footer'
 import './HomePage.scss'
 
@@ -11,7 +11,52 @@ export default function HomePage() {
             <div className="header">
                 <Header/>
             </div>
-            <TestComponent/>
+
+            <div className="card-main-container">
+                <div className="card-container">
+                    <CardComponent
+                    // Normal state
+                        imageUnactive='images/collaboration-unactive.svg'
+                        title='Collaboration'
+                    // On mouseOver
+                        imageActive='images/collaboration-active.svg'
+                        body='C du lorem ipsum ça wallah C du lorem ipsum'
+                    />
+
+                    <div className="card-separator"></div>
+
+                    <CardComponent
+                    // Normal state
+                        imageUnactive='images/collaboration-unactive.svg'
+                        title='Collaboration'
+                        imageActive='images/collaboration-active.svg'
+                        body='C du lorem ça wallah C du lorem ipsum ça wallah'
+                    />
+
+                    <div className="card-separator"></div>
+
+                    <CardComponent
+                    // Normal state
+                        imageUnactive='images/collaboration-unactive.svg'
+                        title='Collaboration'
+                    // On mouseOver
+                        imageActive='images/collaboration-active.svg'
+                        body='C du lorem ça wallah C du lorem ipsum ça wallah'
+                    />
+
+                    <div className="card-separator"></div>
+
+                    <CardComponent
+                    // Normal state
+                        imageUnactive='images/collaboration-unactive.svg'
+                        title='Collaboration'
+                    // On mouseOver
+                        imageActive='images/collaboration-active.svg'
+                        body='C du lorem ça wallah C du lorem ipsum ça wallah'
+                    />
+                </div>
+            </div>
+
             <div className="footer">
                 <Footer/>
             </div>
